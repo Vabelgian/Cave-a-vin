1677c63e74287b8a5f70bfaa5b6f05bf41fa0902
Vabelgian <mathieu.brouckaert@gmail.com>
2026-05-26 10:12:46 +0200

    Add service worker for caching pages

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = 'cave-v1';
+const PAGES = [
+  '/Cave-a-vin/',
+  '/Cave-a-vin/index.html',
+  '/Cave-a-vin/Ma-Cave.html',
+  '/Cave-a-vin/millesimes.html',
+  '/Cave-a-vin/regionsviticoles.html',
+  '/Cave-a-vin/Fenetrededegustation.html',
+  '/Cave-a-vin/decryptage-oenologique.html',
+  '/Cave-a-vin/cepage-du-mois.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(PAGES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
